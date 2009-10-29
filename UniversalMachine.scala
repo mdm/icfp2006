b296c4dca14eebc40faad923aecf66bcee2c781c
mdm <marcmigge@gmx.net>
2009-10-29 23:15:42 +0100

    first working version of um

--- UniversalMachine.scala
+++ UniversalMachine.scala
@@ -1,31 +1,135 @@
-class UniversalMachine {
+package um
+
+class UniversalMachine(program: Array[Int]) {
     private var registers = new Array[Int](8)
     private var arrays = new Array[Array[Int]](1)
     private var finger: Int = 0
-    private var isRunning
+    private var isRunning: Boolean = true
 
-    private def opConditionalMove(a: Int, b: Int, c:Int):Int = {
+    arrays(0) = program
+
+    private def opConditionalMove(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Copy register %d (%08x) to register %d, if register %d (%08x) is not 0. \n\n", b, registers(b), a, c, registers(c)))
 	if (registers(c) != 0) registers(a) = registers(b)
-	++finger
+	finger += 1
     }
 
-    private def opArrayIndex(a: Int, b: Int, c:Int):Int = {
+    private def opArrayIndex(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Copy array location \"%08x[%08x]\" (%08x) to register %d\n\n", registers(b), registers(c), arrays(registers(b))(registers(c)), a))
 	registers(a) = arrays(registers(b))(registers(c))
-	++finger
+	finger += 1
     }
 
-    private def opArrayAmendment(a: Int, b: Int, c:Int):Int = {
+    private def opArrayAmendment(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Copy register %d (%08x) to array location \"%08x[%08x]\"\n\n", c, registers(c), registers(b), registers(c)))
 	arrays(registers(a))(registers(b)) = registers(c)
-	++finger
+	finger += 1
+    }
+
+    private def opAddition(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Add register %d (%08x) to register %d (%08x) and store the result (modulo 2^32) in register %d\n\n", b, registers(b), c, registers(c), a))
+	registers(a) = registers(b) + registers(c)
+	finger += 1
+    }
+
+    private def opMultiplication(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Multiply register %d (%08x) with register %d (%08x) and store the result (modulo 2^32) in register %d\n\n", b, registers(b), c, registers(c), a))
+	registers(a) = registers(b) * registers(c)
+	finger += 1
+    }
+
+    private def opDivision(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Divide register %d (%08x) by register %d (%08x) and store the result (modulo 2^32) in register %d\n\n", b, registers(b), c, registers(c), a))
+	if (registers(c) == 0) fail("ERROR: Division by zero.")
+	val unsigned_b = (registers(b).toLong << 32) >>> 32
+	val unsigned_c = (registers(c).toLong << 32) >>> 32
+	val result = unsigned_b / unsigned_c
+	registers(a) = result.toInt
+	Console.withOut(Console.err)(printf("Result %d (%08x)\n\n", registers(a), registers(a)))
+	finger += 1
+    }
+
+    private def opNotAnd(a: Int, b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Calculate \"register %d (%08x) NAND register %d (%08x)\" and store the result in register %d\n\n", b, registers(b), c, registers(c), a))
+	registers(a) = ~(registers(b) & registers(c))
+	finger += 1
+    }
+
+    private def opHalt() {
+	Console.withOut(Console.err)(printf("Halt the Universal Machine\n\n"))
+	isRunning = false
+	finger += 1
+    }
+
+    private def opAllocation(b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Allocate an array of the size given in register %d (%08x) and store its identifier in register %d\n\n", c, registers(c), b))
+	Console.withOut(Console.err)(println(arrays.length))
+	var identifier = 1
+	while (identifier < arrays.length) {
+	    if (arrays(identifier) == null) {
+		arrays(identifier) = new Array[Int](registers(c))
+		registers(b) = identifier
+		return finger += 1
+	    }
+	    identifier += 1
+	}
+	val box = new Array[Array[Int]](1)
+	box(0) = new Array[Int](registers(c))
+	arrays = arrays ++ box
+	registers(b) = identifier
+	Console.withOut(Console.err)(println(arrays.length))
+	finger += 1
+    }
+
+    private def opAbandonment(c:Int) {
+	Console.withOut(Console.err)(printf("Free array with the identifier given in register %d (%08x)\n\n", c, registers(c)))
+	arrays(registers(c)) = null
+	finger += 1
+    }
+
+    private def opOutput(c:Int) {
+	Console.withOut(Console.err)(printf("Write character given in register %d (%08x) to the console\n\n", c, registers(c)))
+	if (c < 0 || c > 255) fail("ERROR: Illegal output.")
+	print(registers(c).toChar)
+	finger += 1
+    }
+
+    private def opInput(c:Int) {
+	Console.withOut(Console.err)(printf("Read a character from the console and store it in register %d\n\n", c))
+	registers(c) = System.in.read
+	finger += 1
+    }
+
+    private def opLoadProgram(b: Int, c:Int) {
+	Console.withOut(Console.err)(printf("Load program from array with identifier given in register %d (%08x) and start execution at offset given in register %d (%08x)\n\n", b, registers(b), c, registers(c)))
+	if (registers(b) != 0) {
+	    arrays(0) = new Array[Int](arrays(registers(b)).length)
+	    Array.copy(arrays(registers(b)), 0, arrays(0), 0, arrays(0).length)
+	}
+	finger = registers(c)
+    }
+
+    private def opOrthography(operator:Int) {
+	val a = (operator >> 25) & 7
+	registers(a) = operator & 0x01ffffff
+	Console.withOut(Console.err)(printf("Store value \"%08x\" in register %d\n\n", registers(a), a))
+	finger += 1
+    }
+
+    private def fail(error: String) {
+	println(error)
+	opHalt();
     }
 
     def run() {
 	while (isRunning) {
 	    val operator = arrays(0)(finger)
 	    val opcode = operator >>> 28
-	    val a = operator << 1 >> 7 
-	    val b = operator << 1 >> 4 
-	    val c = operator << 1 >> 1 
+	    val a = (operator >> 6) & 7
+	    val b = (operator >> 3) & 7 
+	    val c = operator & 7
+
+	    Console.withOut(Console.err)(printf("Operator #%d: %d (%08x)\nOpcode: %d, Registers: %d (%08x), %d (%08x), %d (%08x)\n", finger, operator, operator, opcode, a, registers(a), b, registers(b), c, registers(c)))
 	    
 	    opcode match {
 		case 0 => opConditionalMove(a, b, c)
@@ -36,7 +140,7 @@ class UniversalMachine {
 		case 5 => opDivision(a, b, c)
 		case 6 => opNotAnd(a, b, c)
 		case 7 => opHalt()
-		case 8 => opAllocation(a, b, c)
+		case 8 => opAllocation(b, c)
 		case 9 => opAbandonment(c)
 		case 10 => opOutput(c)
 		case 11 => opInput(c)
--- um.scala
+++ um.scala
@@ -1,6 +1,48 @@
-import UniversalMachine
+package um
+import java.io.{ByteArrayOutputStream, FileInputStream}
 
 object UM {
     def main(args: Array[String]) {
+	if (args.length == 0) {
+	    println("Usage: scala um.UM <filename>")
+	}
+	else {
+	    val um = new UniversalMachine(pack(args(0)))
+	    um.run()
+	}
+    }
+
+    def pack(filename: String): Array[Int] = {
+	val buffer = new Array[Byte](2048)
+	val is = new FileInputStream(filename)
+	val os = new ByteArrayOutputStream
+
+	var n: Int = 0
+	while (n != -1) {
+	    os.write(buffer, 0, n)
+	    n = is.read(buffer)
+	}
+
+	val bytes = os.toByteArray
+	val words = bytes.length / 4
+	val program = new Array[Int](words)
+	
+	var word = 0
+	while (word < words) {
+	    val offset = 4 * word
+	    program(word) = (bytes(offset) << 24) & 0xff000000
+	    program(word) |= (bytes(offset + 1) << 16) & 0x00ff0000
+	    program(word) |= (bytes(offset + 2) << 8) & 0x0000ff00
+	    program(word) |= bytes(offset + 3) & 0x000000ff
+
+	    if (word == 0) {
+		Console.withOut(Console.err)(printf("Bytes: %2x, %2x, %2x, %2x\n Operator: %d (%08x)\n\n", bytes(offset), bytes(offset + 1), bytes(offset + 2), bytes(offset + 3), program(word), program(word)))
+	    }
+
+	    word += 1
+	}
+	
+	println(words)
+	return program
     }
 }
